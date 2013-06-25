df3caacd2a4fe58e3c6bcb08b39dbcfe9d791bcb
Jan Nemecek <Nemecek.Jan@seznam.cz>
2013-06-25 02:32:53 +0200

    Removed second declaration of cJobManager. There is already one in cJobManager.hpp/.cpp
    Signed-off-by: Jan Nemecek <Nemecek.Jan@seznam.cz>

--- WhoreMasterRenewal/src/cJobs.h
+++ WhoreMasterRenewal/src/cJobs.h
@@ -74,47 +74,6 @@ struct sJobs
 	int m_DependantSkill = 0;
 };
 
-//mainly a list of functions
-class cJobManager
-{
-public:
-	static std::vector<sJobs> job_list;
-	void setup_job_list();
-	bool generic_job(Girl * girl,int job_num,Brothel *,std::string &,int);
-	void WorkFreetime(Girl* girl, Brothel* brothel, int DayNight);
-	void WorkTraining(Girl* girl, Brothel* brothel, int DayNight, std::string& summary);
-	bool WorkBar(Girl* girl, Brothel* brothel, int DayNight, std::string& summary);
-	bool WorkHall(Girl* girl, Brothel* brothel, int DayNight, std::string& summary);
-	bool WorkShow(Girl* girl, Brothel* brothel, int DayNight, std::string& summary);
-	bool WorkWhore(Girl* girl, Brothel* brothel, int DayNight, std::string& summary);
-	bool WorkCleaning(Girl* girl, Brothel* brothel, int DayNight, std::string& summary);
-	bool WorkMatron(Girl* girl, Brothel* brothel, int DayNight, std::string& summary);
-	bool WorkSecurity(Girl* girl, Brothel* brothel, int DayNight, std::string& summary);
-	bool WorkAdvertising(Girl* girl, Brothel* brothel, int DayNight, std::string& summary);
-	bool WorkTorturer(Girl* girl, Brothel* brothel, int DayNight, std::string& summary);
-	void customer_rape(Girl*);
-	bool gang_stops_rape(Girl* girl, sGang *gang, int chance, int day_night);
-	bool girl_fights_rape(Girl*, int);
-	int guard_coverage(std::vector<sGang*> *v = nullptr );
-	bool work_related_violence(Girl*, int, bool);
-	bool explore_catacombs(Girl *);
-	bool beast_capture(Girl * girl,Brothel * brothel,std::string& summary,int);
-	bool work_show(Girl * girl,Brothel * brothel,std::string& summary,char job_id,int DayNight);
-	void update_film(Brothel *);
-	long make_money_films();
-
-	//helpers
-    std::string get_job_name(int id){return job_list[id].m_Name;}
-	std::vector<Girl*> girls_on_job(Brothel *brothel, u_int job_wanted, int day_or_night);
-	Girl **get_training_set(std::vector<Girl*> &v);
-	void do_training(Brothel* brothel, int DayNight);
-	void do_training_set(Girl **girls, int day_night);
-	void do_solo_training(Girl *girl, int day_night);
-	
-private:
-	std::vector<sFilm *> film_list = {};
-};
-
 } // namespace WhoreMasterRenewal
 
 #endif // CJOBS_H_INCLUDED_1524
